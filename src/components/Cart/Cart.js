53d2607123a0d701acc13c3b4bc3a64b9d31448d
Jaswanth Sai <jasvanthsai7@gmail.com>
2023-08-11 10:21:17 +0530

    added cart functions

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,50 @@
-import React from "react";
-import classes from "./Cart.module.css";
-import Modal from "../UI/Modal";
+import { useContext } from 'react';
 
-export default function Cart(props) {
-  const item = [{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(
-    (item) => <li>{item.name}</li>
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {cartCtx.removeItem(id);};
+
+  const cartItemAddHandler = (item) => {cartCtx.addItem({...item, amount: 1});};
+
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
   );
 
   return (
     <Modal hideCartHandler={props.hideCartHandler}>
-      {item}
-      <div>
+      {cartItems}
+      <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button-alt"]} onClick={props.hideCartHandler}>Close</button>
-        <button className={classes.button}>order</button>
+        <button className={classes['button--alt']} onClick={props.hideCartHandler}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
-}
+};
+
+export default Cart;
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classes from './MealItem.module.css'
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
+
 
 export default function MealItem(props) {
-
+const cartCtx=useContext(CartContext)
     const price=`$${props.price.toFixed(2)}`;
+
+const addToCartHandler=amount=>{
+cartCtx.addItem({
+  id:props.id,
+  name:props.name,
+  amount:amount,
+  price:props.price
+})
+}
 
   return (
     <li>
@@ -16,7 +27,7 @@ export default function MealItem(props) {
 
         </div>
         <div>
-            <MealItemForm/>
+            <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
         </div>
     </li>
   )
